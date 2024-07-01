2c7397b5666c764d0d722ec71dd0578fe26e36dd
jrubioh1 <rubiocuri@gmail.com>
2024-07-01 17:32:33 +0200

    20240701-247

--- src/main/java/com/jorge/curso/springboot/backend/backend_products/repositories/ProductRepository.java
+++ src/main/java/com/jorge/curso/springboot/backend/backend_products/repositories/ProductRepository.java
@@ -2,9 +2,11 @@ package com.jorge.curso.springboot.backend.backend_products.repositories;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.jorge.curso.springboot.backend.backend_products.entities.Product;
 
+@CrossOrigin(originPatterns = "http://localhost:5173/")
 @RepositoryRestResource(path = "products")
 public interface ProductRepository extends CrudRepository<Product, Long>{
 
